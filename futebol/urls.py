350f6aceed66ef80ba681c68e2eee0340c11dc3e
IsaacRaffs <124933998+IsaacRaffs@users.noreply.github.com>
2024-03-06 17:30:54 -0300

    Update urls.py pagas

--- futebol/urls.py
+++ futebol/urls.py
@@ -1,7 +1,10 @@
-
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
+
+admin.site.site_header = 'Futebol Arte'
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
